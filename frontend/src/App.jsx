c16da8e89317071740f05ba55418abd8a39332cf
srihithasharma <srihithasharma723@gmail.com>
2024-04-16 20:54:04 +0530

    initial commite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+
+    </>
+  );
+}
+
+export default App;
